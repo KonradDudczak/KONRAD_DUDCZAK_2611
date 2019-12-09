a07f7379df7d20490245214e65b0e1c0a1b97a22
KonradDudczak <konraddd4@gmail.com>
2019-12-09 22:26:56 +0100

    POPRAWA ZADANIA zmiany w funkcji erase

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "tstring.h"
 
 #include<iostream>
@@ -19,42 +18,7 @@ int main ()
   cout<<s1<<endl;
   s1.erase(0,3);
   cout<<s1<<endl;
-  // s1.erase();
-//TString s4("inicjalizacja slowem tym");
-  //TString s3;
-   // TString s3 (s2);
-    // albo TString s3 {s2};
-   // TString s3 = "alfa beta";
-  //s3 = s2;
-//  s3=std::move(s1);
-  //TString s5=fun("jakis napis");
-  //TString s6=s4;
-//for (auto n:s1) {n++;} endl (cout); 
-//for (const auto& n: s1) cout << n <<" - "; cout << endl;
-
-//s1.insert(0, "wstawka");
-cout << "zawartosc obiektu s1: " << s1 << endl;
-
-=======
-#include "tstring.h"
-
-#include<iostream>
-using namespace std;
-using namespace MojeOperatory;
-
-TString fun(const char* c) {
-  return TString(c);
-}
-
-
-
-int main ()
-{ TString s1="inicjalizacja slowem";
-  s1.insert(2, "proba");
-  cout<<s1<<endl;
-  s1.insert(s1.size(), "druga");
-  cout<<s1<<endl;
-  s1.erase(0,3);
+  s1.erase(1,3);
   cout<<s1<<endl;
   // s1.erase();
 //TString s4("inicjalizacja slowem tym");
@@ -72,5 +36,4 @@ int main ()
 //s1.insert(0, "wstawka");
 cout << "zawartosc obiektu s1: " << s1 << endl;
 
->>>>>>> c229828dc004ab1d1ef47285c0a1af1ecf12295a
 }
--- tstring.cpp
+++ tstring.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include"tstring.h"
 
 #include<iostream>
@@ -142,169 +141,19 @@ char* TString:: erase(size_t bpos, size_t leng)
     {delete [] ptr;}
 if (bpos<leng && (bpos>=0 && leng<=len))
 {
-char* tmp = new char [len - (leng-bpos+1)];
-for (size_t i=0; i<=( len - (leng-bpos+1)); i++)
-{tmp[i] = ptr [(leng+1)+i];}
+char* tmp = new char [len - (leng-bpos+1)+1];
+for (size_t i=0; i<bpos;i++)
+{tmp[i]=ptr[i];}
+for (size_t i=0; i<(len -leng);i++)
+{tmp[bpos+i]=ptr[leng+1+i];}
+delete [] ptr ;
+ptr = new char [len - (leng-bpos+1)+1];
 ptr = tmp;
+len = len - (leng-bpos+1);
 return ptr;
 }
 else {throw out_of_range("zle argumenty");}
 
 
     
-=======
-#include"tstring.h"
-
-#include<iostream>
-#include<stdexcept>
-using namespace std;
-TString::TString(const char* s):ptr(nullptr), len(0){ 
-    if(s>0){
-        len =strlen(s);
-        ptr = new char [len +1];
-        strcpy(ptr,s);
-    }
-#ifdef DEBUG
-    cout<<"TString c-tor"<<len<<" - "<<(ptr ? ptr : "pusty")<<endl;
-#endif
-}
-
-TString::~TString(){
-#ifdef DEBUG
-cout<<"TString d-tor"<< len <<" - "<<(ptr ? ptr : "pusty")<<endl;
-#endif
-delete [] ptr;
-
-}
-
-TString::TString(const TString& s) : ptr(nullptr), len(s.len) {
-    if (len>0){
-        ptr = new char [len + 1];
-        strcpy(ptr, s.ptr);
-    }
-
- #ifdef DEBUG
-cout<<"TString cc-tor"<< len <<" - "<<(ptr ? ptr : "pusty")<<endl;
-#endif
-
-}
-
-
-
-
-TString& TString::operator= (const TString& s){
-    if (this != &s) {//if (*this !=s
-    }
-    delete [] ptr;  ptr = nullptr; len=s.len;
-    if(len>0)
-    {
-        ptr = new char [len + 1];
-        strcpy(ptr, s.ptr);
-    }
-
-    #ifdef DEBUG
-cout<<"TString copy operator= "<< len <<" - "<<(ptr ? ptr : "pusty")<<endl;
-#endif
-return *this;
-
-
-}
-
-
-TString& TString::operator= ( TString && s){
-    if (this != &s)  {
-
-    delete [] ptr;  
-    len=s.len;
-    ptr = s.ptr; 
-    s.len = 0;
-    s.ptr = nullptr;
-    
-    }
-#ifdef DEBUG
-cout<<"TString move operator= "<< len <<" - "<<(ptr ? ptr : "pusty")<<endl;
-#endif
-return *this;
-    
-        
-    } 
-
-
-char& TString::operator[](size_t n){
-if (!ptr) throw invalid_argument("pusty obiket");
-if (n >= 0 && n <= len ) return ptr[n];
-throw out_of_range("In TString::operator[] argument out of scope");
-
-}
-
-const char& TString::operator[](size_t n) const {
-
-    if (!ptr) throw invalid_argument("pusty obiket");
-if (n >= 0 && n <= len ) return ptr[n];
-throw out_of_range("In TString::operator[] argument out of scope");
-}
-
-char* TString::insert( size_t pos, const char* c ) {
-if (pos >=0 && pos <= len) {
-size_t oldlen = len;
-len = len+strlen(c);
-char* tmp = new char[ len+1 ];
-strcpy( tmp, ptr );
-for (size_t i=pos; i<pos+strlen(c); ++i) {
-tmp[i] = c[i-pos];
-}
-for (size_t i=pos; i<oldlen; ++i) {
-tmp[i+strlen(c)] = ptr[i];
-}
-delete [] ptr;
-ptr = tmp;
-return ptr;
-} else {
-throw out_of_range("zly argument");
-}
-return ptr;
-cout<<*ptr;
-}
-
-char* TString:: insert(size_t pos, char* c)
-{
-if (pos >=0 && pos <= len) {
-size_t oldlen = len;
-len = len+strlen(c);
-char* tmp = new char[ len+1 ];
-strcpy( tmp, ptr );
-for (size_t i=pos; i<pos+strlen(c); ++i) {
-tmp[i] = c[i-pos];
-}
-for (size_t i=pos; i<oldlen; ++i) {
-tmp[i+strlen(c)] = ptr[i];
-}
-delete [] ptr;
-ptr = tmp;
-return ptr;
-} else {
-throw out_of_range("zly argument");
-}
-return ptr;
-cout<<*ptr;
-
-}
-
-char* TString:: erase(size_t bpos, size_t leng)
-
-{if (bpos==0 && leng==0)
-    {delete [] ptr;}
-if (bpos<leng && (bpos>=0 && leng<=len))
-{
-char* tmp = new char [len - (leng-bpos+1)];
-for (size_t i=0; i<=( len - (leng-bpos+1)); i++)
-{tmp[i] = ptr [(leng+1)+i];}
-ptr = tmp;
-return ptr;
-}
-else {throw out_of_range("zle argumenty");}
-
-
-    
->>>>>>> c229828dc004ab1d1ef47285c0a1af1ecf12295a
 }
