be5dd98934762d899a369a902bc92bc8f705ec01
KonradDudczak <konraddd4@gmail.com>
2020-01-09 18:44:22 +0100

    09.01.20

--- operatory.cpp
+++ operatory.cpp
@@ -22,3 +22,14 @@ s.ptr = nullptr;
 }
 return strumien;
 }
+
+TString MojeOperatory::operator+(const TString& a, const TString& b){
+    TString tmp (a);
+    tmp.insert(tmp.size(), b );
+    return tmp;
+
+}
+
+TString& MojeOperatory::operator+=(TString& a, const TString& b){
+    return a = a+b;
+}
